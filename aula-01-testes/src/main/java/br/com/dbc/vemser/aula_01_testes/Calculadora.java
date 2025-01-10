373658ee36fee730a9c238bad4ec78ed46e3e6f1
RafaelR4mos <rafaelramos0401@gmail.com>
2025-01-10 15:24:02 -0300

    add: aula 01 testes iniciais

--- aula-01-testes/src/main/java/br/com/dbc/vemser/aula_01_testes/Calculadora.java
+++ aula-01-testes/src/main/java/br/com/dbc/vemser/aula_01_testes/Calculadora.java
@@ -9,4 +9,16 @@ public class Calculadora {
     public double subtrair(double a, double b) {
         return a - b;
     }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        if(b == 0) {
+            throw new ArithmeticException("Não é possível dividir qualquer número por zero");
+        }
+
+        return a / b;
+    }
 }
