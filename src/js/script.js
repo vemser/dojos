3ede9345a1b2d9ebca41bc5ebf11582ccffa3186
mateuszzinn <mateuslima2002.ml@gmail.com>
2025-01-27 15:38:45 -0300

    funcao add

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,18 @@
+let pets = []
+
+document.getElementById('form').addEventListener('submit', function(event) {
 const nome = document.getElementById('nome').value;
 const especie = document.getElementById('especie').value;
 const raca = document.getElementById('raca').value;
 const idade = document.getElementById('idade').value;
 const historicoPet = document.getElementById('historico-saude').value;
-const pets = []
-
-document.getElementById('form').addEventListener('submit', function(event) {
     event.preventDefault();
     const input = document.getElementById('form-group')
     addPet(input.value)
-}) // aaa
+    let pet = {nome:nome, especie:especie, raca:raca, idade:idade, historicoPet:historicoPet}
+    pets.push(pet)
+    console.log()
+}) 
+
+
+
