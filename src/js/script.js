aa6b07f2cd7c13763d3259629080a8c4a5b12d26
ThSFernandes <Thsalles99@gmail.com>
2025-01-27 15:21:41 -0300

    pegando ids

---
+++ src/js/script.js
@@ -0,0 +1,8 @@
+const nome = document.getElementById('nome').value;
+const especie = document.getElementById('especie').value;
+const raca = document.getElementById('raca').value;
+const idade = document.getElementById('idade').value;
+const historicoPet = document.getElementById('historico-saude').value;
+
+
+function
