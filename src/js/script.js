4376e35efbae98df59a1d80d30016e5869cd0467
nathanmintegui <nathanberger.rs@hotmail.com>
2025-01-27 16:02:45 -0300

    fix: html structure

--- src/js/script.js
+++ src/js/script.js
@@ -5,14 +5,11 @@ const nome = document.getElementById('nome').value;
 const especie = document.getElementById('especie').value;
 const raca = document.getElementById('raca').value;
 const idade = document.getElementById('idade').value;
+
 const historicoPet = document.getElementById('historico-saude').value;
     event.preventDefault();
     const input = document.getElementById('form-group')
-    addPet(input.value)
     let pet = {nome:nome, especie:especie, raca:raca, idade:idade, historicoPet:historicoPet}
     pets.push(pet)
     console.log()
 }) 
-
-
-
